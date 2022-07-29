dde3dcd51652ded600943b38d103eac3a93da2a8
ChrisSpescha <SpeschaChris@gmail.com>
2022-07-28 22:33:34 -0400

    Working in Portfolio Section

--- src/App.js
+++ src/App.js
@@ -4,16 +4,18 @@ import Layout from './components/Layout'
 import Home from './components/Home'
 import About from './components/About'
 import Contact from './components/Contact';
+import Portfolio from './components/Portfolio';
 
-function App() {
+
+function App({ searchTerm, handleSearch }) {
   return (
     <>
       <Routes>
         <Route path="/" element={<Layout />}>
           <Route index element={<Home />} />
           <Route path="about" element={<About />} />
+          <Route path="portfolio" element={<Portfolio />} />
           <Route path="Contact" element={<Contact />} />
-
         </Route>
       </Routes>
     </>
