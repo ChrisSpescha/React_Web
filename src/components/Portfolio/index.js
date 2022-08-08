737dcb58c079c740a563b3eb26e8bfa8a7db3e89
ChrisSpescha <SpeschaChris@gmail.com>
2022-08-08 16:28:23 -0400

    Handling Conditional Rendering

--- src/components/Portfolio/index.js
+++ src/components/Portfolio/index.js
@@ -6,7 +6,7 @@ import './index.scss'
 function Frontend() {
   return <>
           <div className="App">
-            <ul>
+          
               {frontendProjects.map((project) => (
                 <div key={project.id}>
                   <h1>{project.name}</h1>
@@ -15,45 +15,50 @@ function Frontend() {
                   <br/>
                 </div>
               ))}
-            </ul>
-            
 
           </div>
           </>;
 }
 
 function Backend() {
+  return  <>
+            <Card sx={{ maxWidth: 345 }}>
+              <CardMedia
+                component="img"
+                height="70"
+                image="/static/images/cards/contemplative-reptile.jpg"
+                alt="green iguana"
+              />
+              <CardContent>
+                <Typography gutterBottom variant="h5" component="div">
+                  Lizard
+                </Typography>
+                <Typography variant="body2" color="text.secondary">
+                  Lizards are a widespread group of squamate reptiles, with over 6,000
+                  species, ranging across all continents except Antarctica
+                </Typography>
+              </CardContent>
+              <CardActions>
+                <Button size="small">Share</Button>
+                <Button size="small">Learn More</Button>
+              </CardActions>
+            </Card>
+          </>
+}
+
+function AllDisplay(props) {
   return <>
           <div className="App">
-            <ul>
-              {backendProjects.map((project) => (
-                <div key={project.id}>
+          
+              {allProjects.map((project) => (
+                <div>
                   <h1>{project.name}</h1>
                   <p>{project.tags}</p>
                   <p>{project.link}</p>
                   <br/>
                 </div>
               ))}
-            </ul>
             
-
-          </div>
-          </>;
-}
-
-function AllDisplay() {
-  return <>
-          <div className="App">
-            <ul>
-              {allProjects.map((project) => (
-                <div key={project.id}>
-                  <h1>{project.name}</h1>
-                  <p >{project.tags}</p>
-                  <p >{project.link}</p>
-                  <br/>
-                </div>
-              ))}
-            </ul>
             
 
           </div>
@@ -63,7 +68,7 @@ function AllDisplay() {
 function Greeting(props) {
   const display = props.display;
   if (display === 0) {
-    return <AllDisplay />;
+    return <AllDisplay key={display} />;
   } else if (display === 1){
     return <Frontend />
   } else {
@@ -71,17 +76,17 @@ function Greeting(props) {
 }
 }
 
-function LoginButton(props) {
+function BackEndButton(props) {
   return (
-    <Button variant="outlined" onClick={props.onClick}>
+    <Button size='large' variant="outlined" onClick={props.onClick}>
       Front end
     </Button>
   );
 }
 
-function LogoutButton(props) {
+function FrontEndButton(props) {
   return (
-    <Button variant="outlined" onClick={props.onClick}>
+    <Button size='large' variant="outlined" onClick={props.onClick}>
       Back end
     </Button>
   );
@@ -89,7 +94,7 @@ function LogoutButton(props) {
 
 function AllProjects(props) {
   return (
-    <Button variant="outlined" onClick={props.onClick}>
+    <Button size='large' variant="outlined" onClick={props.onClick}>
       All
     </Button>
   );
@@ -98,17 +103,17 @@ function AllProjects(props) {
 class App extends React.Component {
   constructor(props) {
     super(props);
-    this.handleLoginClick = this.handleLoginClick.bind(this);
-    this.handleLogoutClick = this.handleLogoutClick.bind(this);
+    this.handleFrontEndClick = this.handleFrontEndClick.bind(this);
+    this.handleBackEndClick = this.handleBackEndClick.bind(this);
     this.handleAllClick = this.handleAllClick.bind(this);
     this.state = {display: 0};
   }
 
-  handleLoginClick() {
+  handleFrontEndClick() {
     this.setState({display: 1});
   }
 
-  handleLogoutClick() {
+  handleBackEndClick() {
     this.setState({display: 2});
   }
 
@@ -121,14 +126,20 @@ class App extends React.Component {
  
 
     return (
+      <>
       <div className='portfolio-page'>
-        <div className='text-zone'>
-        <AllProjects onClick={this.handleAllClick}/>
-        <LoginButton onClick={this.handleLoginClick} />
-        <LogoutButton onClick={this.handleLogoutClick} />
+        <div className='tab-zone text-go'>
+          <AllProjects onClick={this.handleAllClick}/>
+          <BackEndButton onClick={this.handleFrontEndClick} />
+          <FrontEndButton onClick={this.handleBackEndClick} />
+        </div>
+      </div>
+      <div className='portfolio-page '>
+        <div className='text-zone text-go'>
         <Greeting display={display} />
         </div>
       </div>
+      </>
     );
   }
 }
